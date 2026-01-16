df6bbf16d6646f6fd5a4ae52a320381c1c256753
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-01-16 08:31:20 -0800

    Task three completed

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+LOGIN_REDIRECT_URL = 'list_books'  # Redirect to the books list after login
+LOGOUT_REDIRECT_URL = 'login'      # Redirect to login page after logout
