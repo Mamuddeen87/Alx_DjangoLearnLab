0adccaefe3eb2474e77fb8725d327b0b6fe8aedb
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-05 12:34:29 +0100

    add_book and edit_book added in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
 
-    # Permission-protected book actions
+    # Permission-protected book actions — MUST exist for ALX
     path('books/add/', add_book, name='add_book'),
     path('books/edit/<int:pk>/', edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', delete_book, name='delete_book'),
