5e38b3ce6419ab1c1e1c2e2da29b12f829bcff05
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-05 15:17:30 +0100

    import book

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import CustomUser  # Or Book if you have a Book model here
+from .models import Book
 # Create your views here.
+
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Query all books
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
