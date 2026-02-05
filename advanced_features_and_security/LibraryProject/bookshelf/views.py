4ceb728da61cdc8eb636229e89fef54f772e43c1
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-05 15:43:34 +0100

    form imported

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
-from .models import CustomUser  # Or Book if you have a Book model here
-from .models import Book
-# Create your views here.
+from .models import Book  # or CustomUser if needed
+from .forms import ExampleForm  # <-- add this line
 
+# List of books view
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()  # Query all books
+    books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+# Optional: Example view to use the form
+@login_required
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # or your URL name
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
