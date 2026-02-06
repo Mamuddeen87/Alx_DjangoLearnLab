a77f9b604125bb90856bc9649212a23a59c3eae0
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-06 11:18:34 +0100

    viewsets added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,14 @@
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
+from rest_framework import viewsets
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     #this provides full CRUD operations for Book Model.
     queryset = Book.objects.all()
     serializer_class = BookSerializer
