067ae9e992e3e0fd17eff32784a47d2d29014326
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-17 20:48:09 +0100

    task 2 done

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedView
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="posts")
