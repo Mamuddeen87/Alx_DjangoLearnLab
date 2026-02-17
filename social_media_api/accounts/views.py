eb0f6c9e2d5bcdc45ec99168fbd89df66d2ba7f5
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-17 18:09:12 +0100

    Api project task 0

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+User = get_user_model()
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "bio": user.bio,
+            "followers_count": user.followers.count(),
+            "following_count": user.following.count(),
+        })
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data["token"])
+        return Response({
+            "token": token.key,
+            "user_id": token.user_id,
+            "username": token.user.username,
+        })
+
