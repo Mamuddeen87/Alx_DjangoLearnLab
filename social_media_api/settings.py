2e60656c7c835eb45cb26694f0d5dd7972900a95
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-18 15:20:54 +0100

    deployment almost ready

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = False #config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 
